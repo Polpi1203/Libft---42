1fe81156c230beea13c0a70dbfcd07ea3307ad6a
Polpi <anthotuigr@gmail.com>
2022-11-03 07:20:28 +0100

    train

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,35 +6,63 @@
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 20:57:18 by polpi             #+#    #+#             */
-/*   Updated: 2022/11/02 21:33:39 by polpi            ###   ########.fr       */
+/*   Updated: 2022/11/03 07:19:46 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(n * -1, fd);
+	}
+	else if (n < 10)
+		ft_putchar_fd((char)(n + '0'), fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+}
+*/
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	*nbr;
 
 	nbr = "0123456789";
-	
-	if (n < 0)
+	if (n == -2147483648)
+	{
+		write (fd, "-2", 2);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
 	{
 		n = n * -1;
 		write (fd, "-", 1);
 	}
-	if (n > 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);	
-	}
-	else if (n < 9)
+	if (n < 10)
 	{
 		write (fd, &nbr[n], 1);
 	}
+	else 
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + 48, fd);
+	}
 }
-
+/*
 int main(void)
 {
-	ft_putnbr_fd(-214748368, 1);
+	ft_putnbr_fd(-2147483648, 1);
 }
+*/
